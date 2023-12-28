efaf3acb07040cd8fabbca76dcdd445a03e452f3
depechejuan <depechejuan@gmail.com>
2023-12-28 09:56:38 +0100

    changes for production

--- src/services/get-user.js
+++ src/services/get-user.js
@@ -1,6 +1,7 @@
 const host = import.meta.env.VITE_API_HOST;
 
 async function getUser() {
+    console.log(host);
     const result = await fetch(`${host}/users`);
     const users = await result.json();
     return users;
