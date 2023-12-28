afd3d99b36b0ccf78573e2dd1b253ae9af8dca22
depechejuan <depechejuan@gmail.com>
2023-12-28 10:14:46 +0100

    testing

--- src/services/get-user.js
+++ src/services/get-user.js
@@ -1,9 +1,10 @@
 const host = import.meta.env.VITE_API_HOST;
 
 async function getUser() {
-    console.log(host);
+    console.log(host + "/users");
     const result = await fetch(`${host}/users`);
     const users = await result.json();
+    console.log(users);
     return users;
 }
 
