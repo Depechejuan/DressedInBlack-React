185f6f3c03bfaa4485658c88cea781cbbee3bc4b
depechejuan <depechejuan@gmail.com>
2024-03-28 14:55:13 +0100

    changes on linktree

--- src/components/LinkTree.jsx
+++ src/components/LinkTree.jsx
@@ -3,17 +3,17 @@ function LinkTree() {
 
     return (
         <section className="LinkTree">
-            <h4>Lista de Enlaces</h4>
-                <a className="btnStack" href="https://entradium.com">ENTRADAS ALBACETE</a>
-                <a className="btnStack" href="https://enlace1.com">TOUR</a>
-                <a className="btnStack" href="https://www.instagram.com/dressedinblackdm/">INSTAGRAM</a>
-                <a className="btnStack" href="https://www.facebook.com/dressedinblackband/">FACEBOOK</a>
-                <a className="btnStack" href="https://www.youtube.com/@Dressedinblackdm">YOUTUBE</a>
-                <a className="btnStack" href="https://www.tiktok.com/@dressedinblackdm">TIKTOK</a>
-                <a className="btnStack" href="https://dressedinblack.netlify.app/">WEBSITE</a>
-                <a className="btnStack" href="https://www.youtube.com/watch?v=LQLwuLQoJZU">VIDEO: NEVER LET ME DOWN AGAIN</a>
-
-                <p className="disclaimer">Todos los enlaces llevan a sitios externos</p>
+            <h4>@Dressed In Black</h4>
+            <h5>Más de 10 años rindiendo Tributo a Depeche Mode</h5>
+            <a className="btnStack" href="https://entradium.com">ENTRADAS ALBACETE</a>
+            <a className="btnStack" href="https://enlace1.com">TOUR</a>
+            <a className="btnStack" href="https://www.instagram.com/dressedinblackdm/">INSTAGRAM</a>
+            <a className="btnStack" href="https://www.facebook.com/dressedinblackband/">FACEBOOK</a>
+            <a className="btnStack" href="https://www.youtube.com/@Dressedinblackdm">YOUTUBE</a>
+            <a className="btnStack" href="https://www.tiktok.com/@dressedinblackdm">TIKTOK</a>
+            <a className="btnStack" href="https://dressedinblack.netlify.app/">WEBSITE</a>
+            <a className="btnStack" href="https://www.youtube.com/watch?v=LQLwuLQoJZU">VIDEO: NEVER LET ME DOWN AGAIN</a>
+            <p className="disclaimer">Todos los enlaces llevan a sitios externos</p>
         </section>
     )
 }
