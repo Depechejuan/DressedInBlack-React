c736cfbb9dfbe984203f91683e584e1c5bb0ef1d
Juan León <depechejuan@gmail.com>
2025-01-28 10:23:46 +0100

    update linktree

--- src/components/LinkTree.jsx
+++ src/components/LinkTree.jsx
@@ -5,10 +5,8 @@ function LinkTree() {
         <section className="LinkTree">
             <h4>@Dressed In Black</h4>
             <h5>Más de 10 años rindiendo Tributo a Depeche Mode</h5>
-            <a className="btnStack" href="https://entradium.com/es/events/tributo-a-depeche-mode-en-albacete-by-dressed-in-black">ENTRADAS ALBACETE - SALA SIDERAL</a>
-            <a className="btnStack" href="https://entradium.com/es/events/dressed-in-black-depeche-mode-tributo">ENTRADAS ALICANTE - CHESTER ROCK & GRILL</a>
-            <a className="btnStack" href="https://www.blacknoteclub.com/event-details/dressed-in-black-tributo-a-depeche-mode-2">ENTRADAS VALENCIA - BLACK NOTE CLUB</a>
-            <a className="btnStack" href="https://enlace1.com">TOUR</a>
+            <a className="btnStack" href="https://entradium.com/es/events/tributo-a-depeche-mode-en-madrid-maravillas-club-dressed-in-black">ENTRADAS MADRID - MARAVILLAS CLUB 15 DE MARZO</a>
+            <a className="btnStack" href="https://www.blacknoteclub.com/event-details/dressed-in-black-tributo-a-depeche-mode-3">ENTRADAS VALENCIA - BLACK NOTE CLUB 25 DE ABRIL</a>
             <a className="btnStack" href="https://www.instagram.com/dressedinblackdm/">INSTAGRAM</a>
             <a className="btnStack" href="https://www.facebook.com/dressedinblackband/">FACEBOOK</a>
             <a className="btnStack" href="https://www.youtube.com/@Dressedinblackdm">YOUTUBE</a>
